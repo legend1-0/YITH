f382cba9f3b0241083d0a0fb36745d836bc2a5ff
Legend1-0 <jacobboluwatife9@gmail.com>
2026-02-02 18:37:36 -0800

    New UPdate

--- main.js
+++ main.js
@@ -71,3 +71,13 @@ successModal.addEventListener("click", (e) => {
     successModal.style.display = "none";
   }
 });
+
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+accordionHeaders.forEach(header => {
+  header.addEventListener("click", () => {
+    const item = header.parentElement;
+
+    item.classList.toggle("active");
+  });
+});
